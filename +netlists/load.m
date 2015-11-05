28ba168a1da118c3c6c804e66df5cd41763eaed3
rogalski <rogalski.91@gmail.com>
2015-11-06 00:07:52 +0100

    first straight-forward implementation on netlists.load() for resistor network and numeric-only nodes

--- +netlists/load.m
+++ +netlists/load.m
@@ -1,18 +1,47 @@
-function [ G, I ] = load( file )
-n = netlists.get_node_count(file);
-G = sparse(n, n);
-I = sparse(n,1);
-
+function [ G ] = load( file )
+resistor_values = [];
+resistor_nodes = [];
+resistors_count = uint8(0);
 
 handle = fopen(file);
-tline = fgetl(handle);
-while ischar(tline)
-    % TODO: Implement parser
-    % Figure out node count to allocate memory correctly
-    disp(tline)
-    tline = fgetl(handle);
+line = fgetl(handle);  % Skip first (comment line)
+
+% Load resistors
+while ischar(line)
+    switch line(1)
+        case 'R'
+            fields = strsplit(line, ' ');
+            node1 = str2double(cell2mat(fields(2)));
+            node2 = str2double(cell2mat(fields(3)));
+            resistor_nodes(2*resistors_count+1) = node1;
+            resistor_nodes(2*resistors_count+2) = node2;
+            resistor_values(resistors_count+1) = str2double(cell2mat(fields(4)));
+            resistors_count = resistors_count + 1;
+        otherwise
+            % ignore line
+    end
+    line = fgetl(handle);
+end
+
+% Fill G with resistors
+n = max(resistor_nodes);
+G = sparse(n, n);
+for k=1:length(resistor_values)
+    m = resistor_nodes(2*k-1);
+    n = resistor_nodes(2*k);
+    gk = 1/resistor_values(k);
+    if m==0 && n==0
+        continue
+    elseif m==0
+        G(n,n) = G(n,n) + gk;
+    elseif n==0
+        G(m,m) = G(m,m) + gk;
+    else
+        G(m,m) = G(m,m) + gk;
+        G(n,n) = G(n,n) + gk;
+        G(m,n) = G(m,n) - gk;
+        G(n,m) = G(n,m) - gk;
+    end
 end
 fclose(handle);
-
 end
-
