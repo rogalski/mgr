4a6458f9c37cc776eed91ba6626469f689f83fad
rogalski <rogalski.91@gmail.com>
2016-02-20 19:43:13 +0100

    delete debug package

--- +debug/disp.m
+++
@@ -1,7 +0,0 @@
-function disp( s )
-global DEBUG
-if all(exist('DEBUG', 'var')) && all(DEBUG)
-    disp(s)
-    snapnow;
-end
-end
--- +debug/spy.m
+++
@@ -1,7 +0,0 @@
-function spy( varargin )
-global DEBUG
-if all(exist('DEBUG', 'var')) && all(DEBUG)
-    spy(varargin{:})
-    snapnow;
-end
-end
