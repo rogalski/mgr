c4d3d8f8edb789cbda0eb69a26f3a001549eed57
rogalski <rogalski.91@gmail.com>
2016-04-03 11:53:13 +0200

    some tweaks in o1 running scripts

--- o1_base_subcircuit.m
+++ o1_base_subcircuit.m
@@ -1,6 +1,8 @@
 % Remove empty nodes
-subC = O1(O1Components==O1_C_ID, O1Components==O1_C_ID);
-
+comp_nodes = O1Components==O1_C_ID;
+n = length(nonzeros(comp_nodes));
+subC = spalloc(n, n, n*200);
+subC(:,:) = O1(comp_nodes, comp_nodes);
 % Re-map terminals
 subCTerminalsMask = zeros(1, length(O1Terminals));
 subCTerminalsMask(O1Terminals) = 1;
--- o1_c1.m
+++ o1_c1.m
@@ -1,34 +1,21 @@
+o_dir = 'o1';
 o1_base_load;
+to_run = 3;
+times = zeros(size(to_run));
+edges = zeros(size(to_run));
+nodes = zeros(size(to_run));
+terminals = zeros(size(to_run));
 
-node_sizes = zeros(1,6);
-edge_sizes = zeros(1,6);
-times = zeros(1,6);
-
-to_run = 1:6;
-
-for k=to_run
-    O1_C_ID = k;
+for circuit=1:numel(to_run)
+    O1_C_ID = to_run(circuit);
     o1_base_subcircuit;
-
-    n = length(subC);
-    e = nnz(tril(subC));
-    t = length(subCTerminals);
-    disp('Node count')
-    disp(n)
-    disp('Edge count')
-    disp(e)
-    disp('Terminal count')
-    disp(t)
-    
+    nodes(circuit) = length(subC);
+    edges(circuit) = nnz(tril(subC));
+    terminals(circuit) = length(subCTerminals);
     G = subC;
     Terminals = subCTerminals;
-    node_sizes(k) = n;
-    edge_sizes(k) = e;
     tic
     reducer;
-    elapsedTime = toc;
-    times(k) = elapsedTime;
+    elapsed_time = toc;
+    times(circuit) = elapsed_time;
 end
-
-% bar(node_sizes(to_run) + edge_sizes(to_run), times(to_run));
-% polyfit(node_sizes(to_run) + edge_sizes(to_run), times(to_run), 2)
