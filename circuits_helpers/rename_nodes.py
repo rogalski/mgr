15cea3bd646661575a0d5fa637d9364ae65ccfac
rogalski <rogalski.91@gmail.com>
2016-04-04 21:30:53 +0200

    tweaks in rename_nodes.py

--- circuits_helpers/rename_nodes.py
+++ circuits_helpers/rename_nodes.py
@@ -4,22 +4,26 @@ with open("edges.txt") as f:
         node1, node2 = l.split()
         seen.add(int(node1))
         seen.add(int(node2))
+        if len(seen) == 100000:
+            break
 
-mapping = {v: i for i, v in enumerate(seen, start=1)}
+mapping = {v: str(i) for i, v in enumerate(seen, start=1)}
 
 with open("edges.txt") as src, open("out_edges.txt", "w") as dst:
     for l in src:
-        node1, node2 = l.split()
-        dst.write(str(mapping[int(node1)]))
+        node1, node2 = (int(s) for s in l.split())
+        if node1 not in seen or node2 not in seen:
+            continue
+        dst.write(str(mapping[node1]))
         dst.write(" ")
-        dst.write(str(mapping[int(node2)]))
+        dst.write(str(mapping[node2]))
         dst.write("\n")
 
 with open("vert.txt") as src, open("out_term.txt", "w") as dst:
     for l in src:
         term = int(l)
         try:
-            dst.write(str(mapping[term]))
+            dst.write(mapping[term])
             dst.write("\n")
         except KeyError:
             pass
@@ -28,5 +32,5 @@ with open("out_mapping.txt", "w") as f:
     for m, t in mapping.items():
         f.write(str(m))
         f.write(str(" "))
-        f.write(str(t))
+        f.write(t)
         f.write("\n")
