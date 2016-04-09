d35211be418338a1def8e29467fbc81fdf4e55ae
rogalski <rogalski.91@gmail.com>
2016-04-09 12:21:25 +0200

    run_reduction: add ignores for code analyser

--- run_reduction.m
+++ run_reduction.m
@@ -1,12 +1,13 @@
+%#ok<*NOPTS>
 clear all
 close all
-CIRCUIT_NAME = 'r_network_int48k_ext8k_res75k_public'
+CIRCUIT_NAME = 'r_network_int46k_ext8k_res67k_public'
 load_rommes
-circuit_info(G, IsExtNode)
+previous_circuit_info = circuit_info(G, IsExtNode)
 % netlists.dump('1.cir', G, ExtNodes);
 % dotfiles.dump('1.dot', G, ExtNodes, 1);
 tic
 reducer;
 toc
-circuit_info(G, IsExtNode)
+reduced_circuit_info = circuit_info(G, IsExtNode)
 % dotfiles.dump('2.dot', G, ExtNodes, 1);
