6bcf3aaf1c90d269858c9f1c1609f66d203804b6
rogalski <rogalski.91@gmail.com>
2016-04-09 12:37:16 +0200

    fix optional argument in run_neato

--- +dotfiles/dump.m
+++ +dotfiles/dump.m
@@ -8,11 +8,15 @@ dotfiles.dump_conductance_matrix(G, handle);
 fprintf(handle,'}\n');
 fclose(handle);
 
+if ~exist('run_neato','var') 
+    run_neato = 0;
+end
+
 % run neato if requested
-if (nargin >= 3 && run_neato)
+if (run_neato)
     [pathstr,name,~] = fileparts(filename);
     output_file = fullfile(pathstr,[name '.png']);
-    cmdline = strjoin({'/usr/local/bin/neato', filename, '-Tpng', ['-o' output_file]}, ' ')
+    cmdline = strjoin({'/usr/local/bin/neato', filename, '-Tpng', ['-o' output_file]}, ' ');
     retcode = system(cmdline);
     if retcode ~= 0
         error('neato:call',...,
