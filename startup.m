0d335cfa70cdb4ee4a74ac1b54ec67a05931c508
rogalski <rogalski.91@gmail.com>
2016-04-11 21:08:33 +0200

    fix paths in startup.m - use Tim Davis metis interfaces

--- startup.m
+++ startup.m
@@ -1,5 +1,5 @@
 % add third-party functions to search path
 addpath third_party/matlab_bgl
 addpath third_party/SuiteSparse/CAMD/MATLAB
-addpath third_party/metis-5.1.0/metismex
+addpath third_party/SuiteSparse/CHOLMOD/MATLAB
 addpath third_party/triconnected-components
