0520e1919e768540166313fe3cd954c5cc5ac336
rogalski <rogalski.91@gmail.com>
2016-04-11 20:25:33 +0200

    load_mrewiens: remove lazy loading

--- load_mrewiens.m
+++ load_mrewiens.m
@@ -1,25 +1,17 @@
-if ~exist('MRewiensTerminals', 'var')
-    MRewiensTerminals = dlmread(['circuits/mrewiens/' CIRCUIT_NAME '/out_term.txt']);
-end
-if ~exist('MRewiensEdges', 'var')
-    MRewiensEdges = dlmread(['circuits/mrewiens/' CIRCUIT_NAME '/out_edges.txt']);
-end
+MRewiensTerminals = dlmread(['circuits/mrewiens/' CIRCUIT_NAME '/out_term.txt']);
+MRewiensEdges = dlmread(['circuits/mrewiens/' CIRCUIT_NAME '/out_edges.txt']);
 m = max(max(MRewiensEdges));
-if ~exist('G', 'var')
-    % NA Matrix
-    G = sparse([MRewiensEdges(:, 1); MRewiensEdges(:, 2); (1:m)'], ...
-                [MRewiensEdges(:, 2); MRewiensEdges(:,1 ); (1:m)'], ...
-                [-1 * ones(1, 2*length(MRewiensEdges)) ones(1, m)], ...
-                m, m, 32 * m);    
-    G(logical(speye(size(G)))) = 1 - sum(G, 2);
-end
-if ~exist('A', 'var')
-    % Adjacency Matrix
-    m = max(max(MRewiensEdges));
-    A = sparse([MRewiensEdges(:, 1); MRewiensEdges(:, 2);], ...
-               [MRewiensEdges(:, 2); MRewiensEdges(:,1 );], ...
-               ones(1, numel(MRewiensEdges)));    
-end
+% NA Matrix
+G = sparse([MRewiensEdges(:, 1); MRewiensEdges(:, 2); (1:m)'], ...
+            [MRewiensEdges(:, 2); MRewiensEdges(:,1 ); (1:m)'], ...
+            [-1 * ones(1, 2*length(MRewiensEdges)) ones(1, m)], ...
+            m, m, 32 * m);    
+G(logical(speye(size(G)))) = 1 - sum(G, 2);
+% Adjacency Matrix
+m = max(max(MRewiensEdges));
+A = sparse([MRewiensEdges(:, 1); MRewiensEdges(:, 2);], ...
+           [MRewiensEdges(:, 2); MRewiensEdges(:,1 );], ...
+           ones(1, numel(MRewiensEdges)));    
 IsExtNode = zeros(1, m);
 IsExtNode(MRewiensTerminals) = 1;
 ExtNodes = MRewiensTerminals;
