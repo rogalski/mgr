019964e24de975e7b9a2cfbf6d34f9131aba1136
rogalski <rogalski.91@gmail.com>
2016-04-11 20:25:24 +0200

    circuit_info: add connected components info

--- circuit_info.m
+++ circuit_info.m
@@ -1,7 +1,10 @@
 function [ info ] = circuit_info( G, IsExtNode )
+non_empty_columns = find(any(G,2));
+
 info = struct;
-info.num_nodes = length(find(any(G,2)));
+info.num_nodes = length(non_empty_columns);
 info.num_external = length(find(IsExtNode == 1));
 info.num_internal = info.num_nodes - info.num_external;
 info.num_resistors = count_resistors(G);
+info.num_conn_components = max(components(adj(G(non_empty_columns, non_empty_columns))));
 end
