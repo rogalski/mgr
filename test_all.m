ec91dafad2d5cc17abf3fd0b57852c2c73a0c018
rogalski <rogalski.91@gmail.com>
2016-04-15 20:40:57 +0200

    test_all tweaks

--- test_all.m
+++ test_all.m
@@ -5,9 +5,11 @@ import matlab.unittest.plugins.CodeCoveragePlugin
 % This is rather unscallable - refactoring may be needed
 s1 = TestSuite.fromFile('test_circuit_info.m');
 s2 = TestSuite.fromFile('test_graph_reduce.m');
-s3 = TestSuite.fromClass(test_netlists);
+s3 = TestSuite.fromClass(?test_netlists);
 suite = [s1, s2, s3];
 
 runner = TestRunner.withTextOutput;
 runner.addPlugin(CodeCoveragePlugin.forFolder(pwd))
+runner.addPlugin(CodeCoveragePlugin.forFolder(fullfile(pwd, '+netlists')))
+runner.addPlugin(CodeCoveragePlugin.forFolder(fullfile(pwd, '+dotfiles')))
 runner.run(suite);
