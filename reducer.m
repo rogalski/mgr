f2a8d596bf885d7b919c1b35a4d4395313ee000c
rogalski <rogalski.91@gmail.com>
2016-04-15 21:07:14 +0200

    nodewise reductor may be selected via options struct

--- reducer.m
+++ reducer.m
@@ -1,14 +1,14 @@
-function out = reducer( G, is_ext_node, ~ )
+function out = reducer( G, is_ext_node, options )
 % REDUCER Performs analysis and reduction of G network.
 % Input:
 %   G - conductance matrix
 %   IsExtNode - mask vector
 % Output:
 %   TBD
+assert(length(is_ext_node) == size(G, 1));
+
 A = adj(G);
 out = struct;
-
-assert(length(is_ext_node) == size(G, 1));
 
 % 1. Compute connected components Gi of G
 connected_components = components(A);
@@ -31,9 +31,18 @@ for conn_comp_i = 1:connected_components_count
 
     after_graph = graph_reduce(conn_comp_G, conn_comp_is_ext_node, conn_comp_A);
     
+    switch options.reorder_strategy
+        case 'none'
+            nodewise_reductor = @nodewise_dummy;
+        case 'nesdis'
+            nodewise_reductor = @nodewise_nesdis_dummy;
+        otherwise
+            nodewise_reductor = @nodewise_camd;
+    end
+    
     % Eliminate nodes ne-by-one
     timer = tic;
-    d = nodewise_camd( after_graph.G, after_graph.is_ext_node, struct );
+    d = nodewise_reductor( after_graph.G, after_graph.is_ext_node, struct );
     out.nodewise_processing_times{conn_comp_i} = toc(timer); 
 
     d.new_nodes = global_context(after_graph.new_nodes(d.new_nodes));
