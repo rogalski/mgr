d6d019f942ab1baf91ff627878cfbd7e877fe61d
rogalski <rogalski.91@gmail.com>
2016-05-03 18:20:13 +0200

    run_spice now invokes ngspice

--- run_spice.m
+++ run_spice.m
@@ -1,12 +1,13 @@
-function [log_file] = run_spice(input_file)
-spice_path = '/Applications/LTspice.app/Contents/MacOS/LTspice';
-cmdline = strjoin({spice_path, '-b', input_file}, ' ');
+function [output_file] = run_spice(input_file)
+[pathstr,name,~] = fileparts(input_file);
+output_file = fullfile(pathstr,[name '.log']);
+
+spice_path = '/usr/local/bin/ngspice';
+cmdline = strjoin({spice_path, '-b', input_file, '-o', output_file}, ' ');
 retcode = system(cmdline);
 if retcode ~= 0
     error('spice:call',...,
         '%s retcode: %d',...,
         cmdline, retcode)
 end
-[pathstr,name,~] = fileparts(input_file);
-log_file = fullfile(pathstr,[name '.log']);
 end
