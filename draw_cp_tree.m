b2c83d272ed3d667f5c472681de4c4ed723def23
rogalski <rogalski.91@gmail.com>
2016-05-27 12:30:56 +0200

    tweaks to draw_cp_tree

--- draw_cp_tree.m
+++ draw_cp_tree.m
@@ -1,5 +1,5 @@
 function draw_cp_tree( cp, cmember )
-treeplot(cp);
+treeplot(cp, 'o black', '- black');
 hold on;
 [x, y, h] = treelayout(cp);
 for i=1:length(x)
@@ -15,4 +15,9 @@ for i=1:length(x)
         'VerticalAlignment', 'top', ...
         'FontSize', 12)
 end
+ylim([min(y) - 0.05, max(y) + 0.05]);
+xlim([min(x) - 0.05, max(x) + 0.05]);
+pbaspect([length(unique(x)), h, 1])
+T = get(gca,'tightinset');
+set(gca,'position',[T(1) T(2) 1-T(1)-T(3) 1-T(2)-T(4)]);
 axis off
