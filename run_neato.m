eab4b4af05ad57df78c26349f7d952c8bc147158
rogalski <rogalski.91@gmail.com>
2016-05-27 12:31:28 +0200

    tweaks to run_neato

--- run_neato.m
+++ run_neato.m
@@ -1,7 +1,10 @@
-function [output_file] = run_neato(filename)
+function [output_file] = run_neato(filename, extra_args)
+if nargin < 2
+   extra_args = '';
+end
 [pathstr,name,~] = fileparts(filename);
 output_file = fullfile(pathstr,[name '.png']);
-cmdline = strjoin({'/usr/local/bin/neato', filename, '-Tpng', ['-o' output_file]}, ' ');
+cmdline = strjoin({'/usr/local/bin/neato', extra_args, filename, '-Tpng', ['-o' output_file]}, ' ');
 retcode = system(cmdline);
 if retcode ~= 0
     error('neato:call',...,
