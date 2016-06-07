01d738fde73732d211e27848c2b0959657130823
rogalski <rogalski.91@gmail.com>
2016-06-07 20:49:55 +0200

    netlists now includes .dc as default simulation

--- +netlists/dump_footer.m
+++ +netlists/dump_footer.m
@@ -1,4 +1,6 @@
 function dump_footer( handle )
-fprintf(handle, '.op\n');
+fprintf(handle, '.dc V1 0 1 10m\n');
+fprintf(handle, '.print dc I(V1)\n');
+fprintf(handle, '.print dc I(V2)\n');
 fprintf(handle, '.end\n');
 end
