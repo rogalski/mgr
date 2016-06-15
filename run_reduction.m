3d54d3412b95dbad6f3e76e8ac9a74eca71f94e7
rogalski <rogalski.91@gmail.com>
2016-06-15 22:59:46 +0200

    run_reduction: clean up

--- run_reduction.m
+++ run_reduction.m
@@ -1,21 +1,29 @@
 %#ok<*NOPTS>
-clear all %#ok<CLALL>
+% clear all %#ok<CLALL>
 close all
-[G, is_ext_node] = load_mrewiens('lr_fake1');
-
-input_circuit_info = circuit_info(G, is_ext_node)
 
 options = struct;
 options.nodewise_algorithm = 'camd';
 options.graph_algorithm = 'standard';
-options.cost_function = @count_resistors;
-options.verbose = 1;
-options.auto_save = 0;
+% options.cost_function = @count_resistors;
+options.nesdis_opts = [param 0 1 0];
+options.verbose = 0;
 
 tic
 output = reducer(G, is_ext_node, options)
-output.options
+output.options;
 toc
 
 to_info = input_for_circuit_composite_info(output);
 output_circuit_info = circuit_composite_info(to_info{:})
+
+if output_circuit_info.num_conn_components == 1
+   t1 = find(is_ext_node, 1);
+   G(t1, t1) = G(t1, t1) + 1;
+   tn1 = find(output.c{1}.new_nodes == t1);
+   output.c{1}.G(tn1, tn1) = output.c{1}.G(tn1, tn1) + 1;
+   check_reduction_correctness(G, is_ext_node, output.c{1}.G, output.c{1}.new_nodes); 
+end
+
+to_dump = input_for_dump(output);
+netlists.dump_composite([circuit_name '_' num2str(param) '.cir'], to_dump{:})
