44e715342b52b1b51240b968a87f00f6ca4fe4ae
rogalski <rogalski.91@gmail.com>
2016-08-17 13:43:45 +0200

    find not trivial reduction targets, use them in basic test suite

--- create_test_suite_basic.m
+++ create_test_suite_basic.m
@@ -1,27 +1,31 @@
 dst_dir = fullfile('test_suites', 'basic');
 
-PURGE = 0;
+PURGE = 1;
 if PURGE && exist(dst_dir,'dir')
     rmdir(dst_dir, 's')
 end
 mkdir(dst_dir)
 
 % mrewiens
+% c = [1:6];
+c = [];
 create_test_suite_for_circuit(@load_mrewiens, 'o1', ...
-                              [1, 2, 3, 6], dst_dir);
+    c, dst_dir);
 % rommes
-c = [1343 1399 93 90 1400 1403 96 86 1402 92 161 85];
+c = [85 86 87 88 90 92 93 96 161 325 1265 1298 1299 1300 1301 1302 1315 1317 1343 1344 1399 1400 1402 1403];
 create_test_suite_for_circuit(@load_rommes, 'r_network_int46k_ext8k_res67k_public',...
-                              c, dst_dir);
+    c, dst_dir);
+c = [1 2 3 4 5 6 12];
 create_test_suite_for_circuit(@load_rommes, 'r_network_int48k_ext8k_res75k_public',...
-                              [1:6 9 12], dst_dir);
+    c, dst_dir);
+c = 1:12;
 create_test_suite_for_circuit(@load_rommes, 'r_network_int50k_ext4k_res94k_public',...
-                              1:12, dst_dir);
+    c, dst_dir);
 % rommes2
-c = [1, 3, 4, 6, 8, 9, 16, 17, 18, 19, 22, 23, 27, 28, 33, 35, 37, 39, 41, 68, 76, 80, 258];
+c = [1 2 3 4 5 6 8 9 10 11 12 15 16 17 18 19 20 21 22 23 24 27 28 29 31 33 35 37 39 40 41 42 43 44 45 46 48 52 53 54 58 62 63 65 68 69 70 72 75 76 78 80 81 83 84 85 86 87 89 91 93 95 97 98 100 101 103 106 107 109 114 118 120 121 122 123 124 127 128 131 137 138 139 141 147 152 155 158 163 171 173 174 175 177 182 196 200 212 214 216 222 224 225 233 242 248 255 258 265 290];
 create_test_suite_for_circuit(@load_rommes2, 'network_b',...
-                              c, dst_dir);
-c = [25, 35, 18, 7, 24, 4, 26, 31, 10, 11, 28, 1, 13];
+    c, dst_dir);
+c = [1 2 4 5 7 9 10 11 12 13 14 15 16 17 18 19 20 23 24 25 26 27 28 29 30 31 32 33 35 36 37 38 40 41 42 43 44 45 49 52 54 55 56 59 60 64 65 66 67 68 70 72 74 79 85 86 87];
 create_test_suite_for_circuit(@load_rommes2, 'network_c',...
-                              c, dst_dir);
+    c, dst_dir);
 disp all_done
---
+++ thesis_print_non_trivial_reduction_targets.m
@@ -0,0 +1,43 @@
+VERBOSE = 0;
+
+circuits = {
+    {@load_rommes, 'r_network_int46k_ext8k_res67k_public'}, ...
+    {@load_rommes, 'r_network_int48k_ext8k_res75k_public'}, ...
+    {@load_rommes, 'r_network_int50k_ext4k_res94k_public'}, ...
+    {@load_rommes2, 'network_b'}, {@load_rommes2, 'network_c'}, ...
+    {@load_mrewiens, 'o1'}
+    };
+
+is_non_trivial = @(i) i.num_nodes >= 80 && i.num_external >= 4 && i.num_internal >= 10;
+
+counter = 0;
+
+for idx=1:numel(circuits)
+    circuit_data_cell_array = circuits(idx);
+    circuit_data = circuit_data_cell_array{1};
+    loader = circuit_data{1};
+    circuit_name = circuit_data{2};
+    [G, is_ext_node] = loader(circuit_name);
+    [c, ~] = components(adj(G));
+    
+    local_found = [];
+    
+    for ci = 1:max(c)
+        s = c==ci;
+        cG = G(s, s);
+        cis_ext_node = is_ext_node(s);
+        
+        info = circuit_info(cG, cis_ext_node);
+        assert(info.num_conn_components == 1)
+        
+        if is_non_trivial(info)
+            counter = counter + 1;
+            local_found = [local_found ci]; %#ok<AGROW>
+            if VERBOSE
+                fprintf(1, '%d. %s component %d\n', counter, circuit_name, ci);
+            end
+        end
+    end
+    disp(circuit_name)
+    disp(mat2str(local_found))
+end
