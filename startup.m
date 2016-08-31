577a82158c87e586986c3922be88a82609a1da0c
rogalski <rogalski.91@gmail.com>
2016-08-31 09:29:01 +0200

    turn off common warnings

--- startup.m
+++ startup.m
@@ -3,3 +3,6 @@ addpath third_party/matlab_bgl
 addpath third_party/SuiteSparse/CAMD/MATLAB
 addpath third_party/SuiteSparse/CHOLMOD/MATLAB
 addpath third_party/triconnected-components
+
+% turn off common (expected) warnings
+warning('off', 'MATLAB:MKDIR:DirectoryExists')
