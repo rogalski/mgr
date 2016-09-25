1896942c1a7577d41220c9d76ade8892cf31fb0f
rogalski <rogalski.91@gmail.com>
2016-09-25 13:29:42 +0200

    graphwiz commands should create eps files

--- +graphviz/run__gv_cmd.m
+++ +graphviz/run__gv_cmd.m
@@ -4,7 +4,7 @@ if nargin < 3
 end
 [pathstr,name,~] = fileparts(filename);
 output_file = fullfile(pathstr,[name '.eps']);
-cmdline = strjoin({['/usr/local/bin/' cmd], extra_args, filename, '-Tpng', ['-o' output_file]}, ' ');
+cmdline = strjoin({['/usr/local/bin/' cmd], extra_args, filename, '-Teps', ['-o' output_file]}, ' ');
 retcode = system(cmdline);
 if retcode ~= 0
     error([cmd ':call'], ...,
